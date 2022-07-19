2400b576f7d13854779b62b1fa2a4999d63903d1
Wisnu Giri <wisnugiri64@gmail.com>
2022-07-19 11:21:41 +0700

    re-update git

---
+++ Front-end/src/App.js
@@ -0,0 +1,13 @@
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Hello World!
+        </p>
+    </div>
+  );
+}
+
+export default App;
